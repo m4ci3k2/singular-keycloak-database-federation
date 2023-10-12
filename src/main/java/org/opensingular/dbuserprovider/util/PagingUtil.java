25dd99a720f7f4bdcae03d87504d77d0a33fb099
m4ci3k2 <m4ci3k2@users.noreply.github.com>
2023-10-12 15:05:57 +0200

    version for keycloak 21

--- src/main/java/org/opensingular/dbuserprovider/util/PagingUtil.java
+++ src/main/java/org/opensingular/dbuserprovider/util/PagingUtil.java
@@ -2,7 +2,7 @@ package org.opensingular.dbuserprovider.util;
 
 import org.hibernate.dialect.Dialect;
 import org.hibernate.dialect.pagination.LimitHandler;
-import org.hibernate.query.spi.Limit;
+import org.hibernate.engine.spi.RowSelection;
 import org.opensingular.dbuserprovider.DBUserStorageException;
 import org.opensingular.dbuserprovider.persistence.RDBMS;
 
@@ -29,18 +29,22 @@ public class PagingUtil {
     public static String formatScriptWithPageable(String query, Pageable pageable, RDBMS RDBMS) {
 
         final Dialect dialect = RDBMS.getDialect();
-        Limit limit = new Limit(pageable.firstResult, pageable.maxResults);
+
+        RowSelection rowSelection = new RowSelection();
+        rowSelection.setFetchSize(pageable.maxResults);
+        rowSelection.setFirstRow(pageable.firstResult);
+        rowSelection.setMaxRows(pageable.maxResults);
 
         String escapedSQL = escapeQuestionMarks(query);
 
         StringBuilder processedSQL;
         try {
             LimitHandler limitHandler = dialect.getLimitHandler();
-            processedSQL = new StringBuilder(limitHandler.processSql(escapedSQL, limit));
-            int col = 1;
+            processedSQL = new StringBuilder(limitHandler.processSql(escapedSQL, rowSelection));
+            int                                 col       = 1;
             PreparedStatementParameterCollector collector = new PreparedStatementParameterCollector();
-            col += limitHandler.bindLimitParametersAtStartOfQuery(limit, collector, col);
-            limitHandler.bindLimitParametersAtEndOfQuery(limit, collector, col);
+            col += limitHandler.bindLimitParametersAtStartOfQuery(rowSelection, collector, col);
+            limitHandler.bindLimitParametersAtEndOfQuery(rowSelection, collector, col);
 
             Map<Integer, Object> parameters = collector.getParameters();
             for (int i = 1; i <= parameters.keySet().size(); i++) {
